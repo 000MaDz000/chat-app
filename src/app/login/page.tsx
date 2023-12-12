04b2ebcfe41da3cbd556223f450c84e35269d8ac
mostafa mohammed <000madz000@gmail.com>
2023-12-12 20:26:05 +0200

    make login form inside the login page in the middle of the screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "../_ui/login-form";
 
 export default function LoginPage() {
     return (
-        <div className="login-page-container min-w-full min-h-screen">
+        <div className="login-page-container min-w-full min-h-screen flex items-center justify-center">
             <LoginForm />
         </div>
     )
