9bdec201d10cd7fde103728a5066a7923f0f877e
mostafa mohammed <000madz000@gmail.com>
2023-12-29 21:09:11 +0200

    adding function that returns session data using session id

--- src/utils/get-session-data.ts
+++ src/utils/get-session-data.ts
@@ -3,8 +3,8 @@ import path from "path";
 import { DefaultSessionData } from "./session";
 
 const sessfolder = path.resolve("sessions");
-export default async function getSessionData(sessionId: string): Promise<DefaultSessionData | {}> {
-    if (!sessionId) return {};
+export default async function getSessionData(sessionId: string): Promise<DefaultSessionData> {
+    if (!sessionId) return {} as DefaultSessionData;
     const sessFile = path.join(sessfolder, sessionId) + ".json";
     const sessObject = JSON.parse((await readFile(sessFile)).toString());
     return sessObject;
