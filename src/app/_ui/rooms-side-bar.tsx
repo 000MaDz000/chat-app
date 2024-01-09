dce9cb773647e796f02eb9df0d485fd837a4e233
mostafa mohammed <000madz000@gmail.com>
2024-01-09 23:20:33 +0200

    adding overflow scroll

--- src/app/_ui/rooms-side-bar.tsx
+++ src/app/_ui/rooms-side-bar.tsx
@@ -12,35 +12,22 @@ export default async function RoomsSideBar() {
     let userRooms = await Room.getUserRooms(new ObjectId(id));
 
     return (
-        <div className={"bg-gray-50 p-5 flex flex-col gap-3 w-1/5 shadow-md min-w-max sticky top-0 max-h-screen"}>
-            <div className="">
+        <div className={"bg-gray-50 p-5 pt-0 flex flex-col gap-3 w-1/5 shadow-md min-w-max sticky top-0 max-h-screen overflow-y-auto"}>
+
+            <div className="sticky top-0 flex flex-col gap-3 bg-inherit pt-5 pb-5">
                 <h1 className="text-xl font-bold">LOGO</h1>
+                <hr />
+                <CreateRoomButton className="w-full" noDefaultWidth />
             </div>
-            <hr />
-            <p></p>
 
-            <CreateRoomButton className="w-full" noDefaultWidth />
 
-            <div>
+            <div className="flex flex-col gap-2">
                 {
                     userRooms.map(r => <RoomLink roomName={r.roomname} key={r._id.toString()} />)
                 }
-            </div>
-            {/* 
-            <div className="rooms-links flex flex-col gap-2 font-serif">
-                <RoomLink roomName="rom1" />
-                <RoomLink roomName="rom2" />
-                <RoomLink roomName="rom3" />
-                <RoomLink roomName="rom4" />
-                <RoomLink roomName="rom5" />
-                <RoomLink roomName="rom6" />
-                <RoomLink roomName="rom7" />
-                <RoomLink roomName="rom8" />
-                <RoomLink roomName="rom9" />
-                <RoomLink roomName="rom10" />
-                <RoomLink roomName="rom11" />
 
-            </div> */}
+            </div>
+
         </div>
     )
 }
