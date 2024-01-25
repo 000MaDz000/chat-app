e5ecdb6de255996036b86335778297d64ddcf9f4
mostafa mohammed <000madz000@gmail.com>
2024-01-25 09:32:35 +0200

    make max-resend-code is 3

--- src/app/_ui/resend-code-button.tsx
+++ src/app/_ui/resend-code-button.tsx
@@ -7,7 +7,7 @@ export default function ReSendCodeButton() {
     const [counter, setCounter] = useState(0);
 
     return (
-        <button aria-disabled className={"text-blue-600 hover:underline hover:text-sky-700 transition-colors text-md font-semibold text-left" + (counter >= 3 ? " cursor-not-allowed" : "")} onClick={() => {
+        <button className={"text-blue-600 hover:underline hover:text-sky-700 transition-colors text-md font-semibold text-left" + (counter >= 3 ? " cursor-not-allowed" : "")} onClick={() => {
             if (counter < 3) {
                 reSendCode();
                 setCounter(counter + 1);
